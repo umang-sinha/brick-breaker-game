d3dceac00e5f00bc6735c1d1df310ddbae4481d5
umang-sinha <umangsinha12@gmail.com>
2021-05-31 08:43:57 +0530

    implemented interface into the GamePlay class

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -1,5 +1,28 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GamePlay extends JPanel {
+public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
