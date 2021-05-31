a3f12f13c97bdc5e95662e00878afb34bf0525c6
umang-sinha <umangsinha12@gmail.com>
2021-05-31 08:37:08 +0530

    created empty JFrame

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        JFrame jFrame = new JFrame();
+        jFrame.setBounds(10, 10, 700, 600);
+        jFrame.setTitle("Brick Breaker Game");
+        jFrame.setResizable(false);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
