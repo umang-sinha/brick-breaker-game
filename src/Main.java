af8cced54025c66355422136f3d4b6fb756bb1bf
umang-sinha <umangsinha12@gmail.com>
2021-05-31 08:41:41 +0530

    added JPanel to JFrame

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ public class Main {
     public static void main(String[] args){
 
         JFrame jFrame = new JFrame();
+        GamePlay gamePlay = new GamePlay();
         jFrame.setBounds(10, 10, 700, 600);
         jFrame.setTitle("Brick Breaker Game");
         jFrame.setResizable(false);
         jFrame.setVisible(true);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.add(gamePlay);
     }
 }
